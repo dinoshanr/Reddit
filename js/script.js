15d7ec3de8bcac7f11cd229a5dceeecffa8bbde5
ggatling <gerda.gatling@gmail.com>
2019-09-11 20:18:01 -0400

    added modal for update user

--- js/script.js
+++ js/script.js
@@ -112,14 +112,23 @@ function displayAll(event) {
 };
 displayAll();
 
-function updateProfile(event){
-  event.preventDefault()
+// Get the modal
+let modal = document.getElementById("myModal");
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
-
-function updateProfile(event){
-  event.preventDefault()
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
-
-function updateProfile(event){
-  event.preventDefault()
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
